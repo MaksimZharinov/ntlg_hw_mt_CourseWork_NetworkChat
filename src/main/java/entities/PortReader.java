20c21d95c65a613cab93448e808a6e86bc25815f
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-13 10:04:10 +0700

    create PortReader

---
+++ src/main/java/entities/PortReader.java
@@ -0,0 +1,14 @@
+package entities;
+
+import interfaces.Readable;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+
+public class PortReader implements Readable {
+
+    @Override
+    public void read() {
+
+    }
+}
