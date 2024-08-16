5a79d2a74b5f21dcfaa3adcc167cd0800ae9d7d9
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-16 09:11:20 +0700

    change Server

--- src/main/java/entities/Server.java
+++ src/main/java/entities/Server.java
@@ -1,16 +1,73 @@
 package entities;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     private int port;
+    private Socket client;
+    private ServerSocket server;
+    private DataInputStream in;
+    private DataOutputStream out;
 
     public Server(String path) {
         PortReader portReader = new PortReader(path);
         port = portReader.getPort();
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Start SERVER in " + port + " port");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    public Socket connectClient() {
+        try {
+            client = server.accept();
+            in = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return client;
+    }
+
+    public String clientName() {
+        String name = null;
+        try {
+            out.writeUTF("Enter your name: ");
+            out.flush();
+            name = in.readUTF();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return name;
+    }
+
+    public void closeClient(Socket client,
+                            BufferedReader in,
+                            BufferedWriter out) {
+        try {
+            in.close();
+            out.close();
+            client.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 
     public int getPort() {
         return port;
+    }
+
+    public DataInputStream getIn() {
+        return in;
+    }
+
+    public DataOutputStream getOut() {
+        return out;
     }
 
 }
