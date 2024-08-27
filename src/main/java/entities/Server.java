b851f46db39119681ac7838aaf7aa6d74bc06121
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-27 09:54:52 +0700

    change Readable, PortReader, Server

--- src/main/java/entities/Server.java
+++ src/main/java/entities/Server.java
@@ -1,5 +1,85 @@
 package entities;
 
-public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Server {
+    private int port = -1;
+    private Socket client = null;
+    private ServerSocket server = null;
+    private String message = null;
+    private String log_message = null;
+
+    public boolean readPort(String settingsFile) {
+        if (settingsFile.isEmpty() || settingsFile == null) {
+            return false;
+        }
+        try(BufferedReader reader = new BufferedReader(new FileReader(settingsFile))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if(line.contains("port=")) {
+                    String[] strings = line.split("=");
+                    port = Integer.parseInt(strings[strings.length - 1]);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+            return false;
+        }
+        if (port != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public boolean startServer() {
+        if (port == -1) {
+            return false;
+        }
+        else {
+            try {
+                server = new ServerSocket(port);
+                System.out.println("Start SERVER in port: " + port);
+                return true;
+            } catch (IOException e) {
+                System.err.println(e);
+                return false;
+            }
+        }
+    }
+
+    public boolean connectClient() {
+        if (server == null) {
+            return false;
+        }
+        try {
+            System.out.println("New user is trying to connect...");
+            client = server.accept();
+            if (client == null) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+            return false;
+        }
+    }
+
+    public boolean sendToAll(ConcurrentHashMap<String, PrintWriter> clients, String msg) {
+        for (Map.Entry<String, PrintWriter> entry : clients.entrySet()) {
+            PrintWriter out = entry.getValue();
+            out.println(msg);
+        }
+        return true;
+    }
 }
--- src/main/java/interfaces/Readable.java
+++
@@ -1,6 +0,0 @@
-package interfaces;
-
-public interface Readable {
-
-    void read();
-}
