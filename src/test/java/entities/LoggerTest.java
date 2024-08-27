6bc9ffec04626e830dd4c7453c62606dacbdca25
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-28 06:51:51 +0700

    create LoggerTest

---
+++ src/test/java/entities/LoggerTest.java
@@ -0,0 +1,31 @@
+package entities;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoggerTest {
+    private Logger logger;
+
+    @BeforeEach
+    void setUp() {
+        logger = new Logger("src/test/java/testResources/test_log.txt");
+    }
+
+    @Test
+    void setName() {
+
+        assertFalse(logger.setName(""));
+        assertTrue(logger.setName("test_name"));
+    }
+
+    @Test
+    void log() {
+
+//        logger.setName("test_log");
+
+        assertTrue(logger.log("test_message"));
+    }
+}
