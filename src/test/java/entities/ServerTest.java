7978d9a402a2c86fda3c763faecbf2c579570c7e
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-27 10:23:09 +0700

    create ServerTest

---
+++ src/test/java/entities/ServerTest.java
@@ -0,0 +1,34 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    private String settingsPath = "src/main/resources/settings.txt";
+
+    @Test
+    void getPort() {
+    }
+
+    @Test
+    void getClient() {
+    }
+
+    @Test
+    void readPort() {
+    }
+
+    @Test
+    void startServer() {
+    }
+
+    @Test
+    void connectClient() {
+    }
+
+    @Test
+    void sendToAll() {
+    }
+}
