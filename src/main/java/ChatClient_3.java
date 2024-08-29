e8b9e8341c3139b989c08c96a3f1b01140198ac2
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-29 18:48:33 +0700

    change ChatClients

--- src/main/java/ChatClient_3.java
+++ src/main/java/ChatClient_3.java
@@ -2,8 +2,10 @@ import entities.Client;
 import entities.Logger;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ChatClient_3 {
 
@@ -18,6 +20,46 @@ public class ChatClient_3 {
     private static Scanner keyboard;
 
     public static void main(String[] args) {
+        client.readPort(SETTINGS_FILE);
+        client.connect(HOST);
+        in = client.getIn();
+        out = client.getOut();
+        keyboard = new Scanner(System.in);
+        AtomicBoolean flag = new AtomicBoolean(true);
 
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    if (!flag.get()) {
+                        in.close();
+                        client.closeClient();
+                        break;
+                    }
+                    if (in.ready()) {
+                        String messFormServer = in.readLine();
+                        System.out.println(messFormServer);
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                if (keyboard.hasNext()) {
+                    String mess = keyboard.nextLine();
+                    if (mess.equalsIgnoreCase(EXIT)) {
+                        out.println(mess);
+                        keyboard.close();
+                        out.close();
+                        flag.set(false);
+                        break;
+                    }
+                    out.println(mess);
+                }
+            }
+        }).start();
     }
 }
