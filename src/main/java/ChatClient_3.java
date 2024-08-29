90b4a918f464574b198314d9c6c8ceca6365f0eb
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-29 09:23:41 +0700

    change ChatServer, ChatClients

--- src/main/java/ChatClient_3.java
+++ src/main/java/ChatClient_3.java
@@ -1,2 +1,23 @@
+import entities.Client;
+import entities.Logger;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class ChatClient_3 {
+
+    private static final String HOST = "127.0.0.1";
+    private static final String SETTINGS_FILE = "src/main/resources/settings.txt";
+    private static final String LOG_FILE = "src/main/resources/log_client_3.txt";
+    private static final String EXIT = "/exit";
+    private static Client client = new Client();
+    private static Logger logger = new Logger(LOG_FILE);
+    private static BufferedReader in;
+    private static PrintWriter out;
+    private static Scanner keyboard;
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,2 +1,13 @@
+import entities.Logger;
+import entities.Server;
+
 public class ChatServer {
+    private static final String SETTINGS_FILE = "src/main/resources/settings.txt";
+    private static final String LOG_FILE = "src/main/resources/log_server.txt";
+    private static Server server = new Server();
+    private static Logger logger = new Logger(LOG_FILE);
+
+    public static void main(String[] args) {
+
+    }
 }
