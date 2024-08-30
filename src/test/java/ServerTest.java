1ec1f8278660f6df01a0ec3a4f55eccd764a4051
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-08-30 15:38:24 +0700

    create Tests

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @Test
+    void sendMessToAll() {
+    }
+
+    @Test
+    void waitMessAndSend() {
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void sendMsg() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getName() {
+    }
+}
